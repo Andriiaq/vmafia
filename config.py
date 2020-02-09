dd593148e7e1eee9403e019af89e06b94ea9f1cb
Андрій <AndriiUA@outlook.com>
2020-02-09 20:10:39 +0200

    Release 4

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 token = '998012707:AAHnS8mLrmVOq2oIF1ZZUE-3bFHWVlKMAtk'
 database_url = 'postgres://wommmmevwhfpyr:1b5d073a1c84d111c9969c64494ba324f5260049ce5b9f0672885ded3616fcbd@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d44hnvrm79hna6'
-group_id = -1001229855041
+group_id = -1001200252082
